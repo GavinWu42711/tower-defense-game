fb07ff727bf7426b15dc93e5a6dbbe361414cb5c
GavinWu42711 <gavinwu42711@gmail.com>
2026-06-27 16:49:19 -0400

    Setting up enemy
    Setting up the base enemy class

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,21 @@
-extends Node2D
+#Enemy class
 
-#testing
+extends Node
+
+class_name Enemy
+
+@export var health:int
+@export var damage:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+#What happens when the enemy dies (typically only needs to dequeue itself)
+func die() -> void:
+	self.queue_free()
+	
